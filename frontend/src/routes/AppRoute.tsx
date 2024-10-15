593feae6500441ff2cc77084d85872006c2ce516
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2024-10-15 10:54:25 +0000

    change in app.js

--- frontend/src/routes/AppRoute.tsx
+++ frontend/src/routes/AppRoute.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import React from "react";
 import Login from "../pages/Login";
 
 const AppRoutes: React.FC = () => {
