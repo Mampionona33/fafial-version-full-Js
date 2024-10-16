e02c3805fa4e7ffa937aed0a6c08cba662312a63
Mampionona <50689325+Mampionona33@users.noreply.github.com>
2024-10-16 07:47:22 +0000

    création du composant calendar

---
+++ test-components/src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import AppCalendar from "./components/AppCalendar";
+
+function App() {
+  return (
+    <>
+      <div>
+        <AppCalendar />
+      </div>
+    </>
+  );
+}
+
+export default App;
