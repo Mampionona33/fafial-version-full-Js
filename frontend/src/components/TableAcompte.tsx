29cef9fe14611771e197e509a847e15b38206730
Mampionona <50689325+Mampionona33@users.noreply.github.com>
2024-11-06 11:33:32 +0000

    create table acompte

---
+++ frontend/src/components/TableAcompte.tsx
@@ -0,0 +1,9 @@
+const TableAcompte = () => {
+  return (
+    <div className="flex flex-col py-10 px-20 items-center justify-center bg-slate-50">
+      <p>TableAcompte</p>
+    </div>
+  );
+};
+
+export default TableAcompte;
--- frontend/src/pages/Staf/StaffPageListAcompt.tsx
+++ frontend/src/pages/Staf/StaffPageListAcompt.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import HeaderStaffPagelistAcompt from "../../components/HeaderStaffPagelistAcompt.tsx";
-
+import TableAcompte from "../../components/TableAcompte.tsx";
 
 const StaffPageListAcompt: React.FC = () => {
-
   return (
-    <div className="flex flex-col py-10 px-20 items-center justify-center">
-      <HeaderStaffPagelistAcompt/>
-      <p>StafPageListeAcompte</p>
+    <div className="flex flex-col py-10 px-20 items-center justify-center gap-4">
+      <HeaderStaffPagelistAcompt />
+      <TableAcompte />
     </div>
   );
-}
+};
 
-export default StaffPageListAcompt
+export default StaffPageListAcompt;
