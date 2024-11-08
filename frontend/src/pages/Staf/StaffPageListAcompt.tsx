807d78c4d945cb0f2030f2e864913112576b64a7
Mampionona33 <ramamps33@gmai.com>
2024-11-08 22:01:58 +0300

    fix tableau acompte

--- frontend/src/pages/Staf/StaffPageListAcompt.tsx
+++ frontend/src/pages/Staf/StaffPageListAcompt.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import HeaderStaffPagelistAcompt from "../../components/HeaderStaffPagelistAcompt";
 import TableAcompte from "../../components/TableAcompte";
 import { Link, useSearchParams } from "react-router-dom";
@@ -19,7 +19,7 @@ const StaffPageListAcompt: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [pagination, setPagination] = useState({
     page: Number(searchParams.get("page")) || 1,
-    pageSize: 5,
+    pageSize: Number(searchParams.get("pageSize")) || 5,
     pageCount: 0,
   });
   const annee = Number(searchParams.get("annee")) || new Date().getFullYear();
@@ -40,31 +40,32 @@ const StaffPageListAcompt: React.FC = () => {
     }),
     columnHelper.accessor("datePrevue", {
       header: () => "Date prévue de paiement",
-      cell: (info) => {
-        const value = format(new Date(info.getValue()), "dd/MM/yyyy");
-        return <div>{value}</div>;
-      },
+      cell: (info) => (
+        <div>{format(new Date(info.getValue()), "dd/MM/yyyy")}</div>
+      ),
     }),
     columnHelper.accessor("statut", {
       header: () => "Statut",
       cell: (info) => {
-        const value = info.getValue();
-        const datePrevue = info.row.original.datePrevue;
-        const enRetard = new Date(datePrevue) < new Date();
-        if (value === "EN_ATTENTE") {
-          return (
-            <div
-              className={`${
-                enRetard
+        const { statut, datePrevue } = info.row.original;
+        const isLate = new Date(datePrevue) < new Date();
+        return (
+          <div
+            className={`text-center font-bold py-1 px-4 rounded-full ${
+              statut === "EN_ATTENTE"
+                ? isLate
                   ? "text-red-500 bg-red-100"
                   : "text-green-500 bg-green-100"
-              } text-center font-bold py-1 px-4 rounded-full`}
-            >
-              {enRetard ? "En retard" : "En cours"}
-            </div>
-          );
-        }
-        return <div>{value}</div>;
+                : ""
+            }`}
+          >
+            {statut === "EN_ATTENTE"
+              ? isLate
+                ? "En retard"
+                : "En cours"
+              : statut}
+          </div>
+        );
       },
     }),
     columnHelper.accessor("montant", {
@@ -74,21 +75,18 @@ const StaffPageListAcompt: React.FC = () => {
     columnHelper.accessor("id", {
       header: () => "Action",
       cell: (info) => {
-        const value = info.getValue();
-        const status = info.row.original.statut;
+        const { id, statut } = info.row.original;
         return (
-          <div>
-            <Link
-              to={`/staf/acompte/${value}`}
-              className={`${
-                status === "EN_ATTENTE"
-                  ? "bg-slate-800 hover:bg-slate-700"
-                  : "bg-blue-500 hover:bg-blue-700"
-              } text-white font-bold py-2 px-4 rounded`}
-            >
-              {status === "EN_ATTENTE" ? "Payer" : "Consulter"}
-            </Link>
-          </div>
+          <Link
+            to={`/staf/acompte/${id}`}
+            className={`text-white font-bold py-2 px-4 rounded ${
+              statut === "EN_ATTENTE"
+                ? "bg-slate-800 hover:bg-slate-700"
+                : "bg-blue-500 hover:bg-blue-700"
+            }`}
+          >
+            {statut === "EN_ATTENTE" ? "Payer" : "Consulter"}
+          </Link>
         );
       },
     }),
@@ -96,113 +94,78 @@ const StaffPageListAcompt: React.FC = () => {
 
   const table = useReactTable({
     data: listeAcompte || [],
-    columns: columns,
+    columns,
     getCoreRowModel: getCoreRowModel(),
     manualPagination: true,
   });
 
-  useEffect(() => {
-    const fetchData = async () => {
+  const updateSearchParams = useCallback(
+    (page: number, pageSize: number) => {
+      setSearchParams({
+        page: String(page),
+        annee: String(annee),
+        mois: String(mois),
+        pageSize: String(pageSize),
+      });
+    },
+    [setSearchParams, annee, mois]
+  );
+
+  const fetchData = useCallback(
+    async (page: number, pageSize: number) => {
       setLoading(true);
       try {
-        const { status, data } = await AcompteService.getAll({
-          annee: Number(annee),
-          mois: Number(mois),
-          page: page,
-          pageSize: pageSize,
-        });
-        if (status === 200) {
-          setListAcompte(data.acomptes);
-          setPagination((prev) => ({
-            ...prev,
-            page: data.pagination.currentPage,
-            pageSize: data.pagination.pageSize,
-            pageCount: data.pagination.totalPages,
-          }));
+        if (pageSize && page) {
+          const { status, data } = await AcompteService.getAll({
+            annee,
+            mois,
+            page,
+            pageSize,
+          });
+          if (status === 200) {
+            setListAcompte(data.acomptes);
+            setPagination((prev) => ({
+              ...prev,
+              page: data.pagination.currentPage,
+              pageSize: data.pagination.pageSize,
+              pageCount: data.pagination.totalPages,
+            }));
+          }
         }
       } catch (error) {
         console.error(error);
       } finally {
         setLoading(false);
       }
-    };
-    fetchData();
-  }, [
-    annee,
-    mois,
-    pagination.page,
-    pagination.pageSize,
-    setLoading,
-    page,
-    pageSize,
-  ]);
+    },
+    [annee, mois, setLoading]
+  );
 
-  const handlePageChange = async (newPage: number) => {
-    console.log("Page Change - page:", newPage, "pageSize:");
+  useEffect(() => {
+    fetchData(pagination.page, pagination.pageSize);
+  }, [fetchData, pagination.page, pagination.pageSize]);
 
-    setPagination((prev) => ({
-      ...prev,
-      page: newPage,
-      pageSize: pageSize,
-    }));
+  const handlePageChange = useCallback(
+    (newPage: number) => {
+      setPagination((prev) => ({ ...prev, page: newPage }));
+      updateSearchParams(newPage, pageSize);
+      fetchData(newPage, pageSize);
+    },
+    [fetchData, pageSize, updateSearchParams]
+  );
 
-    // Manually trigger a re-fetch after updating the page state
-    try {
-      const { status, data } = await AcompteService.getAll({
-        annee: Number(annee),
-        mois: Number(mois),
-        page: newPage,
-        pageSize: pageSize,
-      });
-      if (status === 200) {
-        setListAcompte(data.acomptes);
-        setPagination({
-          page: data.pagination.currentPage,
-          pageSize: data.pagination.pageSize,
-          pageCount: data.pagination.totalPages,
-        });
-        setSearchParams({
-          page: String(newPage),
-          annee: String(annee),
-          mois: String(mois),
-          pageSize: String(pageSize),
-        });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleRowsPerPageChange = async (newRowsPerPage: number) => {
-    try {
-      setPagination((prev) => ({
-        ...prev,
-        pageSize: newRowsPerPage,
-      }));
-      const { status, data } = await AcompteService.getAll({
-        annee: Number(annee),
-        mois: Number(mois),
+  const handleRowsPerPageChange = useCallback(
+    (newPageSize: number) => {
+      setPagination({
         page: 1,
-        pageSize: newRowsPerPage,
+        pageSize: newPageSize,
+        pageCount: pagination.pageCount,
       });
-      if (status === 200) {
-        setListAcompte(data.acomptes);
-        setPagination({
-          page: data.pagination.currentPage,
-          pageSize: data.pagination.pageSize,
-          pageCount: data.pagination.totalPages,
-        });
-        setSearchParams({
-          page: String(1),
-          annee: String(annee),
-          mois: String(mois),
-          pageSize: String(newRowsPerPage),
-        });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+      updateSearchParams(1, newPageSize);
+      fetchData(page, newPageSize);
+    },
+    [fetchData, pagination.pageCount, updateSearchParams, page]
+  );
 
   return (
     <div className="flex flex-col py-10 px-20 items-center justify-center gap-4">
