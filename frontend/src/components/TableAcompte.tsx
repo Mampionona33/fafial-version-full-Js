0abb86041891d3ee5c317ef4f6331c9d7f134ff7
Mampionona33 <ramamps33@gmai.com>
2024-11-09 19:29:32 +0300

    fini mise en place filtre acompte par colonne

--- frontend/src/components/TableAcompte.tsx
+++ frontend/src/components/TableAcompte.tsx
@@ -1,38 +1,104 @@
 import React from "react";
-import { flexRender, Table } from "@tanstack/react-table";
+import { Column, flexRender, Table } from "@tanstack/react-table";
 import { Acompte } from "../interfaces/AcompteInterface";
 
 interface TableAcompteProps {
   table: Table<Acompte>;
 }
 
+// A typical debounced input react component
+function DebouncedInput({
+  value: initialValue,
+  onChange,
+  debounce = 500,
+  ...props
+}: {
+  value: string | number;
+  onChange: (value: string | number) => void;
+  debounce?: number;
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange">) {
+  const [value, setValue] = React.useState(initialValue);
+
+  React.useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value);
+    }, debounce);
+
+    return () => clearTimeout(timeout);
+  }, [value, onChange, debounce]);
+
+  return (
+    <input
+      {...props}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+}
+
+function Filter({ column }: { column: Column<Acompte, unknown> }) {
+  const columnFilterValue = column.getFilterValue();
+
+  return (
+    <DebouncedInput
+      type="text"
+      value={(columnFilterValue ?? "") as string}
+      onChange={(value) => column.setFilterValue(value)}
+      placeholder={`Search...`}
+      className="w-36 border shadow rounded px-2 py-1"
+    />
+  );
+}
+
 const TableAcompte: React.FC<TableAcompteProps> = ({ table }) => {
+  const rows = table.getRowModel().rows;
+
   return (
     <div className="flex flex-col p-10 items-center justify-center bg-slate-50 w-full overflow-auto">
-      {/* Affichage de la table avec les entêtes et les données */}
-      {table.getRowModel().rows.length > 0 ? (
-        <table className="min-w-full text-sm">
-          <thead>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <th
-                    key={header.id}
-                    className="text-left bg-slate-100 px-4 py-2 text-slate-800"
-                  >
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
+      <table className="min-w-full text-sm">
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id} colSpan={header.colSpan}>
+                  {header.isPlaceholder ? null : (
+                    <div className="flex items-start flex-col px-2">
+                      <div
+                        {...{
+                          className: header.column.getCanSort()
+                            ? "cursor-pointer select-none text-left px-2 py-1"
+                            : "",
+                          onClick: header.column.getToggleSortingHandler(),
+                        }}
+                      >
+                        {flexRender(
                           header.column.columnDef.header,
                           header.getContext()
                         )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody>
-            {table.getRowModel().rows.map((row) => (
+                        {{
+                          asc: " 🔼",
+                          desc: " 🔽",
+                        }[header.column.getIsSorted() as string] ?? null}
+                      </div>
+                      {header.column.getCanFilter() ? (
+                        <div>
+                          <Filter column={header.column} />
+                        </div>
+                      ) : null}
+                    </div>
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {rows.length > 0 ? (
+            rows.map((row) => (
               <tr key={row.id}>
                 {row.getVisibleCells().map((cell) => (
                   <td key={cell.id} className="border-b py-2 px-4">
@@ -40,12 +106,19 @@ const TableAcompte: React.FC<TableAcompteProps> = ({ table }) => {
                   </td>
                 ))}
               </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <p>Aucun acompte disponible</p>
-      )}
+            ))
+          ) : (
+            <tr>
+              <td
+                colSpan={table.getAllColumns().length}
+                className="text-center py-4"
+              >
+                Aucun résultat trouvé
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
--- frontend/src/pages/Staf/StaffPageListAcompt.tsx
+++ frontend/src/pages/Staf/StaffPageListAcompt.tsx
@@ -8,11 +8,60 @@ import {
   createColumnHelper,
   useReactTable,
   getCoreRowModel,
+  FilterFn,
+  SortingFn,
+  sortingFns,
+  ColumnFiltersState,
 } from "@tanstack/react-table";
 import { Acompte } from "../../interfaces/AcompteInterface";
 import { format } from "date-fns";
 import AppPagination from "../../components/AppPagination";
 import { useAuth } from "../../hooks/useAuth";
+import {
+  RankingInfo,
+  rankItem,
+  compareItems,
+} from "@tanstack/match-sorter-utils";
+import { getFilteredRowModel } from "@tanstack/react-table";
+
+declare module "@tanstack/react-table" {
+  //add fuzzy filter to the filterFns
+  interface FilterFns {
+    fuzzy: FilterFn<unknown>;
+  }
+  interface FilterMeta {
+    itemRank: RankingInfo;
+  }
+}
+
+// Define a custom fuzzy filter function that will apply ranking info to rows (using match-sorter utils)
+const fuzzyFilter: FilterFn<unknown> = (row, columnId, value, addMeta) => {
+  // Rank the item
+  const itemRank = rankItem(row.getValue(columnId), value);
+
+  // Store the itemRank info
+  addMeta({
+    itemRank,
+  });
+
+  // Return if the item should be filtered in/out
+  return itemRank.passed;
+};
+
+const fuzzySort: SortingFn<Acompte> = (rowA, rowB, columnId) => {
+  let dir = 0;
+
+  // Only sort by rank if the column has ranking information
+  if (rowA.columnFiltersMeta[columnId]) {
+    dir = compareItems(
+      rowA.columnFiltersMeta[columnId].itemRank,
+      rowB.columnFiltersMeta[columnId].itemRank
+    );
+  }
+
+  // Provide an alphanumeric fallback for when the item ranks are equal
+  return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir;
+};
 
 const StaffPageListAcompt: React.FC = () => {
   const { setLoading } = useLoading();
@@ -29,77 +78,100 @@ const StaffPageListAcompt: React.FC = () => {
   const mois = Number(searchParams.get("mois")) || new Date().getMonth() + 1;
   const page = Number(searchParams.get("page")) || 1;
   const pageSize = Number(searchParams.get("pageSize")) || 5;
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
 
   const columnHelper = createColumnHelper<Acompte>();
 
-  const columns = [
-    columnHelper.accessor("reservation.reference", {
-      header: () => "Reference de la réservation",
-      cell: (info) => <div>{info.getValue()}</div>,
-    }),
-    columnHelper.accessor("reservation.nomOrganisation", {
-      header: () => "Nom du client",
-      cell: (info) => <div>{info.getValue()}</div>,
-    }),
-    columnHelper.accessor("datePrevue", {
-      header: () => "Date prévue de paiement",
-      cell: (info) => (
-        <div>{format(new Date(info.getValue()), "dd/MM/yyyy")}</div>
-      ),
-    }),
-    columnHelper.accessor("statut", {
-      header: () => "Statut",
-      cell: (info) => {
-        const { statut, datePrevue } = info.row.original;
-        const isLate = new Date(datePrevue) < new Date();
-        return (
-          <div
-            className={`text-center font-bold py-1 px-4 rounded-full ${
-              statut === "EN_ATTENTE"
+  const columns = React.useMemo(
+    () => [
+      columnHelper.accessor("reservation.reference", {
+        header: () => "Reference de la réservation",
+        cell: (info) => <div className="text-left">{info.getValue()}</div>,
+        filterFn: "equalsString",
+      }),
+      columnHelper.accessor("reservation.nomOrganisation", {
+        header: () => "Nom du client",
+        cell: (info) => <div>{info.getValue()}</div>,
+        filterFn: "includesString",
+      }),
+      columnHelper.accessor("datePrevue", {
+        header: () => "Date prévue de paiement",
+        cell: (info) => (
+          <div>{format(new Date(info.getValue()), "dd/MM/yyyy")}</div>
+        ),
+        sortingFn: fuzzySort,
+        enableColumnFilter: false,
+      }),
+      columnHelper.accessor("statut", {
+        header: () => "Statut",
+        cell: (info) => {
+          const { statut, datePrevue } = info.row.original;
+          const isLate = new Date(datePrevue) < new Date();
+          return (
+            <div
+              className={`text-center font-bold py-1 px-4 rounded-full ${
+                statut === "EN_ATTENTE"
+                  ? isLate
+                    ? "text-red-500 bg-red-100"
+                    : "text-green-500 bg-green-100"
+                  : ""
+              }`}
+            >
+              {statut === "EN_ATTENTE"
                 ? isLate
-                  ? "text-red-500 bg-red-100"
-                  : "text-green-500 bg-green-100"
-                : ""
-            }`}
-          >
-            {statut === "EN_ATTENTE"
-              ? isLate
-                ? "En retard"
-                : "En cours"
-              : statut}
-          </div>
-        );
-      },
-    }),
-    columnHelper.accessor("montant", {
-      header: () => "Montant",
-      cell: (info) => <div>{info.getValue()}</div>,
-    }),
-    columnHelper.accessor("id", {
-      header: () => "Action",
-      cell: (info) => {
-        const { id, statut } = info.row.original;
-        return (
-          <Link
-            to={`/staf/acompte/${id}`}
-            className={`text-white font-bold py-2 px-4 rounded ${
-              statut === "EN_ATTENTE"
-                ? "bg-slate-800 hover:bg-slate-700"
-                : "bg-blue-500 hover:bg-blue-700"
-            }`}
-          >
-            {statut === "EN_ATTENTE" ? "Payer" : "Consulter"}
-          </Link>
-        );
-      },
-    }),
-  ];
+                  ? "En retard"
+                  : "En cours"
+                : statut}
+            </div>
+          );
+        },
+        enableColumnFilter: false,
+      }),
+      columnHelper.accessor("montant", {
+        header: () => "Montant",
+        cell: (info) => <div>{info.getValue()}</div>,
+      }),
+      columnHelper.accessor("id", {
+        header: () => "Action",
+        cell: (info) => {
+          const { id, statut } = info.row.original;
+          return (
+            <Link
+              to={`/staf/acompte/${id}`}
+              className={`text-white font-bold py-2 px-4 rounded ${
+                statut === "EN_ATTENTE"
+                  ? "bg-slate-800 hover:bg-slate-700"
+                  : "bg-blue-500 hover:bg-blue-700"
+              }`}
+            >
+              {statut === "EN_ATTENTE" ? "Payer" : "Consulter"}
+            </Link>
+          );
+        },
+        enableColumnFilter: false,
+      }),
+    ],
+    [columnHelper]
+  );
 
   const table = useReactTable({
     data: listeAcompte || [],
     columns,
     getCoreRowModel: getCoreRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    globalFilterFn: "fuzzy",
+    getFilteredRowModel: getFilteredRowModel(),
     manualPagination: true,
+    filterFns: {
+      fuzzy: fuzzyFilter,
+    },
+    state: {
+      columnFilters,
+    },
+    debugTable: true,
+    debugHeaders: true,
   });
 
   const fetchData = useCallback(async () => {
